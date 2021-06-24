b4c4fa76ac2c4f9359d5c8abce53594be7f1a8b4
JuliusCedar <49333042+JuliusCedar@users.noreply.github.com>
2021-06-24 10:40:19 -0700

    Player Health SetHealth function added

--- Exploration/Assets/Scenes/PlayerControls/scripts/PlayerHealth.cs
+++ Exploration/Assets/Scenes/PlayerControls/scripts/PlayerHealth.cs
@@ -28,25 +28,30 @@ public class PlayerHealth : MonoBehaviour
 
     }
 
-     void OnCollisionEnter(Collision other)
+    void OnCollisionEnter(Collision other)
     {
         if (isDead == false)
         {
             if (other.gameObject.CompareTag("Enemy"))
             {
-                playerHealth = playerHealth - EnemyDamage;
-                //        healthText.SetText("Health: " + playerHealth);
-                HealthText.GetComponent<Text>().text = "Health: " + playerHealth;
-                Debug.Log("Health: " + playerHealth);
-
-                if (playerHealth <= 0)
-                {
-                    //            endText.SetText("Game Over!");
-                    HealthText.GetComponent<Text>().text = "Game Over!";
-                    Debug.Log("Game Over!");
-                    isDead = true;
-                }
+                SetHealth(playerHealth - EnemyDamage)
             }
         }
     }
+
+    public void SetHealth(int health)
+    {
+        Text text = HealthText.GetComponent<Text>();
+        playerHealth = health;
+        if (playerHealth <= 0)
+        {
+            isDead = true;
+            text.text = "Game Over!";
+        }
+        else
+        {
+            text.text = "Health: " + playerHealth;
+        }
+    }
+
 }
