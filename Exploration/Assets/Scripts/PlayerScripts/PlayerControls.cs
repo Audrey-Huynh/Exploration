18f52f8db2a9a616a42548953274a5e263ac337e
ishdpk <ishdpk20@gmail.com>
2021-06-25 13:39:13 -0500

    edit

--- Exploration/Assets/Scripts/PlayerScripts/PlayerControls.cs
+++ Exploration/Assets/Scripts/PlayerScripts/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scenes/Scripts/PlayerScripts/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerScripts/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
